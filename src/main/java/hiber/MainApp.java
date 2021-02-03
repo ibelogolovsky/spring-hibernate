950bea6170b08309c1989ade6d31ad30351c968f
Igor Belogolovsky <igor@belogolovsky.net>
2021-02-03 15:22:19 +0300

    Added cars table

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,6 +20,12 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User(
+              "Michael",
+              "Schumacher",
+              "msch@f1.com",
+              new Car("Mercedes-Benz", 1)
+      ));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +33,11 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         Car car = user.getCar();
+         if (car != null) {
+            System.out.println("Car = " + car.getModel() +
+                    ", series: " + car.getSeries());
+         }
          System.out.println();
       }
 
